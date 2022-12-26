bdff5da0888811219638f1d764086722b1170249
sachi <sachi@vishwa>
2022-12-26 11:53:15 +0530

    first commit

---
+++ src/demo/class2.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class class2 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is a git project file");
+		System.out.println("code by sachin");
+
+}
+}
